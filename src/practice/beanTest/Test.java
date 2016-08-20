b978ecb9534f4c32e218de119a5cf877df755a7e
Doctor邓 <Doctor@DESKTOP-6099SU7>
2016-08-20 23:43:15 +0800

    @ImpoetResource加载配置文件信息练习

--- src/practice/beanTest/Test.java
+++ src/practice/beanTest/Test.java
@@ -9,7 +9,11 @@ public class Test {
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("spring-bean-test.xml");
 		/*Person person = (Person)ac.getBean("getChinese");*/
-		Person person = (Person)ac.getBean("chinese");
-		person.sayHello();
+		/*Person person = (Person)ac.getBean("chinese");
+		person.sayHello();*/
+		MyDriver myDriver = (MyDriver)ac.getBean("myDriver");
+		System.out.println("userName:"+myDriver.getUserName());
+		System.out.println("url:"+myDriver.getUrl());
+		System.out.println("password:"+myDriver.getPassword());
 	}
 }
