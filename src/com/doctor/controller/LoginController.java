26251bf3bdad2ebeb97516556729698f9c0be172
Administrator <Administrator@503-15>
2016-08-20 09:11:08 +0800

    在公共类中获取request response session获取成功

--- src/com/doctor/controller/LoginController.java
+++ src/com/doctor/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.doctor.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.doctor.util.CommonUtil;
 /**
  * 登录控制类
  * @author Doctor邓
@@ -9,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/login")
-public class LoginController {
+public class LoginController extends CommonUtil{
 	
 	/**
 	 * 跳转到用户登录页面 
@@ -17,6 +22,12 @@ public class LoginController {
 	 */
 	@RequestMapping("/userLogin")
 	public String userLogin(){
+		return "login";
+	}
+	@RequestMapping("/testLogin")
+	public String testLogin(HttpServletRequest request, HttpServletResponse response){
+		System.out.println("类的:"+this.request+"===当前:"+request);
+		System.out.println("类的:"+this.response+"===当前:"+response);
 		return "login";
 	}
 }
